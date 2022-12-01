5afcbbb216ddac049e61fc86a26ffc993cfe7028
HasieEst <ayrtonm0@gmail.com>
2022-12-01 08:47:19 +0200

    "Refactoring existing code to UI.js and Weather.js"

---
+++ classes/Weather.js
@@ -0,0 +1,11 @@
+class Weather{
+    constructor(city) {
+        this.city = city
+        this.key = '7283fe926a327977fed55a5cb6165e9b'
+    }
+
+    async getWeather(){
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        return await response.json()
+    }
+}
